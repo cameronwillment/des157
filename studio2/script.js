68b8fb3eab40e04db1bb4888246b4c1cf2965e5b
Cameron Willment <cmwillment@ucdavis.edu>
2017-02-02 16:27:39 -0800

    Updated blog and studio3

--- studio2/script.js
+++ studio2/script.js
@@ -14,35 +14,49 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   //define process function
     function processForm() {
-    myMove();
+      myMove();
 
-    console.log("processForm");
-    //store user name in a variable
-    var input1  = document.f.input1.value;
-    var input2  = document.f.input2.value;
-    var input3  = document.f.input3.value;
-    var input4  = document.f.input4.value;
-    var input5  = document.f.input5.value;
+      console.log("processForm");
+      //store user name in a variable
+      var input1  = document.f.input1.value;
+      var input2  = document.f.input2.value;
+      var input3  = document.f.input3.value;
+      var input4  = document.f.input4.value;
+      var input5  = document.f.input5.value;
 
 
-    var myMsg=document.getElementById('myMsg');
+      var myMsg=document.getElementById('myMsg');
 
-    myMsg.innerHTML = "Near " +input1 +" lived a man named John<br>"
-    + "Who " + input2 + " untill early dawn<br>"
-    + "Then one "+input3+" day<br>"
-    + "His " +input4 + " made him stay<br>"
-    + "Now John just "+input5 + " on his lawn.";
-     //prevent page from reloading
-    return false;
+      myMsg.innerHTML = "Near " +input1 +" lived a man named John<br>"
+      + "Who " + input2 + " untill early dawn<br>"
+      + "Then one "+input3+" day<br>"
+      + "His " +input4 + " made him stay<br>"
+      + "Now John just "+input5 + " on his lawn.";
+      //prevent page from reloading
+      return false;
 
     }
     function hideLim(){
       console.log("Hide");
-      myMsg.style.display = 'none';
+      // myMsg.style.display = 'none';
+      // var elem = document.getElementById("animate");
+      // elem.style.display = 'none';
       var elem = document.getElementById("animate");
-      elem.style.display = 'none';
+      var pos  = 383;
+      var id = setInterval(frame, 5);
+      function frame() {
+        if (pos == 150) {
+          clearInterval(id);
+        } else {
+          pos--;
+        // elem.style.top = 'px'- pos;
+        elem.style.top = '50px';
 
+        }
+      }
     }
+
+
     function myMove() {
       console.log("move");
       var elem = document.getElementById("animate");
---
+++ studio3/script.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+console.log('reading');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    // all other js here
+
+    //capture the submit event
+
+      // var album = document.getElementsByClassName("album");
+      // album.addEventListener("click",function(){
+      //   document.getElementById('TPAB').innerHTML = "Hello World!";
+      // });
+      function expandPic(){
+        console.log("fwen");
+        alert("hey buddeh!");
+      }
+
+
+
+});
