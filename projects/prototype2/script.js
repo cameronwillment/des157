7f8d77495d0c4d5cfed8896cad6bfcd5708ccb3c
Cameron Willment <cmwillment@ucdavis.edu>
2017-03-09 14:33:18 -0800

    SAF

---
+++ projects/prototype2/script.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+console.log('reading');
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    // all other js here
+
+    //capture the submit event
+
+
+  //define process function
+    function processForm() {
+
+      function fun1(){
+        cosole.log("clicked");
+      }
+
+      console.log("processForm");
+
+}
+});
